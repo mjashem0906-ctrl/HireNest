a0191674919996de6b6935156aa2ab02975f8553
jobbridgekarnataka <jobbridgekarnataka@gmail.com>
2026-05-20 16:23:42 +0530

    workbox limit to 3mb - vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,67 @@
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
+// import path from 'path'
+// import { VitePWA } from 'vite-plugin-pwa';
 
-// // https://vite.dev/config/
+
 // export default defineConfig({
-//   plugins: [react()],
+//   plugins: [
+//     react(),
+//     VitePWA({
+//       registerType: 'autoUpdate',
+//       manifest: {
+//         name: 'JOB BRIDGE Karnataka',
+//         short_name: 'JOB BRIDGE',
+//         description: 'Connecting Seekers and recruiters seamlessly in Karnataka.',
+//         start_url: '/',
+//         display: 'standalone',
+//         background_color: '#ffffff',
+//         theme_color: '#1E3A8A',
+//         icons: [
+//           {
+//             src: '/icons/icon-512x512.png',
+//             sizes: '512x512',
+//             type: 'image/png'
+//           },
+//           {
+//             src: '/icons/icon-192x192.png',
+//             sizes: '192x192',
+//             type: 'image/png'
+//           }
+//         ]
+//       }
+//     })
+//   ],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
 // })
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { VitePWA } from 'vite-plugin-pwa';
-
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+
+      workbox: {
+        maximumFileSizeToCacheInBytes: 3000000 // 3 MB
+      },
+
       manifest: {
         name: 'JOB BRIDGE Karnataka',
         short_name: 'JOB BRIDGE',
@@ -40,6 +85,7 @@ export default defineConfig({
       }
     })
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
